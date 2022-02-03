2c24a41cde3f87904f68da02247a3e832234a0eb
draganet <3417237276@qq.com>
2022-02-03 15:36:35 +0800

    add header

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './home.scss';
+import headerBg from '../images/header.png';
+
 
 export const Home = () => {
   return (
-    <div className="x">
+    <div className="home">
+      <header style={{backgroundImage: `url(${headerBg})`}}/>
     </div>
   );
 };
