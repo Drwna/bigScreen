025f46c88762262e89e836713ea99c62a986e156
draganet <3417237276@qq.com>
2022-02-04 17:01:30 +0800

    chart-5 完成

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,6 +5,8 @@ import {Chart1} from '../components/chart-1';
 import {Chart2} from '../components/chart-2';
 import {Chart3} from '../components/chart-3';
 import {Chart4} from '../components/chart-4';
+import {Chart5} from '../components/chart-5';
+import {Chart6} from '../components/chart-6';
 
 export const Home = () => {
 
@@ -21,8 +23,12 @@ export const Home = () => {
           <Chart3/>
           <Chart4/>
         </section>
-        <section className="bordered section3"></section>
-        <section className="bordered section4"></section>
+        <section className="bordered section3">
+          <Chart5/>
+        </section>
+        <section className="bordered section4">
+          <Chart6/>
+        </section>
         <section className="bordered section5"></section>
       </main>
       <footer>
