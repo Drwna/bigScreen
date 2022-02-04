7b2d4985a129d97c2c86006ee0923ce958a112e9
draganet <3417237276@qq.com>
2022-02-04 21:37:44 +0800

    布局 chart7 chart8 chart9

---
+++ src/components/chart-8.tsx
@@ -0,0 +1,18 @@
+import React, {useEffect, useRef} from 'react';
+import * as echarts from 'echarts';
+
+const Chart8 = () => {
+  const divRef = useRef(null);
+  useEffect(() => {
+    const myChart = echarts.init(divRef.current);
+  }, []);
+
+  return (
+    <div className="年龄段-图1">7
+      <div ref={divRef} className="chart">
+
+      </div>
+    </div>
+  );
+};
+export {Chart8};
